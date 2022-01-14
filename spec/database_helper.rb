27efb1eb7c39c626de8dd3152983b7495a2e89a0
Nastia Gorokhova-Alekseeva <ngoral@304.ru>
2022-01-14 20:05:42 +0300

    Заменяем sqlite на pg

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -1,13 +1,21 @@
+# frozen_string_literal: true
+
 require 'logger'
 
 class TestApplication < Rails::Application; end
 
 ActiveRecord::Base.logger = Logger.new('/tmp/dj.log')
-ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => '/tmp/jobs.sqlite')
+ActiveRecord::Base.establish_connection(
+  adapter: 'postgresql',
+  host: '127.0.0.1',
+  database: 'paperclip_test',
+  encoding: 'utf8',
+  username: 'postgres',
+  port: 5432
+)
 ActiveRecord::Migration.verbose = false
 
 ActiveRecord::Schema.define do
-
   create_table :delayed_jobs, :force => true do |table|
     table.integer  :priority, :default => 0
     table.integer  :attempts, :default => 0
@@ -23,9 +31,7 @@ ActiveRecord::Schema.define do
   create_table :stories, :force => true do |table|
     table.string :text
   end
-
 end
-
 
 # Purely useful for test cases...
 class Story < ActiveRecord::Base
